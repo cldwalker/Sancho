39fbabb22a13190b6330cc59d1e6ee267bb6080f
Gabriel Horner <gabriel.horner@gmail.com>
2014-09-12 17:01:51 -0400

    Add cmd to open grimoire url

--- src/lt/plugins/inc_clojure.cljs
+++ src/lt/plugins/inc_clojure.cljs
@@ -38,16 +38,32 @@
 (defn ->crossclj-url [ns var]
   (str "http://crossclj.info/fun/" ns "/" var ".html"))
 
-(defn open-crossclj [{:keys [result]}]
+;; from https://github.com/clojure-grimoire/grimoire/blob/cf69630e38f4f9c2c94351d1e8ef1547b61d9567/resources/API.md
+(defn munge-grimoire-var [s]
+  (-> s
+      (replace "?" "_QMARK_")
+      (replace "." "_DOT_")
+      (replace "/" "_SLASH_")
+      (replace #"^_*" "")
+      (replace #"_*$" "")))
+
+(defn ->grimoire-url [ns var]
+  ;; Hardcode latest version - not worth calculating version and
+  ;; determing if grimoire supports it
+  (str "http://grimoire.arrdem.com/1.6.0/" ns "/" (munge-grimoire-var var)))
+
+(defn open-resolved-var [->var-url {:keys [result]}]
   (let [[_ ns var] (re-find #"^#'(\S+)/(\S+)$" result)]
     (if (and ns var)
-      (tab-open-url (->crossclj-url ns var))
+      (tab-open-url (->var-url ns var))
       (notifos/set-msg! (str "Invalid clojure var: " result) {:class "error"}))))
+
+(def open-crossclj (partial open-resolved-var ->crossclj-url))
+(def open-grimoire (partial open-resolved-var ->grimoire-url))
 
 (behavior ::clj-result.callback
           :triggers #{:editor.eval.clj.result.callback}
           :reaction (fn [editor result]
-                      ;; (prn "RESULTING" result)
                       (if-let [callback (some->> (get-in result [:meta :callback])
                                                  (resolve-fn lt.plugins.inc-clojure))]
                         (callback (-> result :results first))
@@ -58,18 +74,25 @@
   [editor code callback-kw]
   (let [info (assoc (:info @editor)
                :code code
-               ;; :print-length (object/raise-reduce editor :clojure.print-length+ nil)
                :meta {:callback callback-kw
                       :result-type :callback})]
     (object/raise clojure/clj-lang :eval! {:origin editor :info info})))
 
 (cmd/command {:command :inc-clojure.open-crossclj-url
-              :desc "IncClojure: Opens crossclj page for current symbol"
+              :desc "IncClojure: Open crossclj page for current symbol"
               :exec (fn []
                       (let [ed (pool/last-active)]
                         (eval-code ed
                                    (str "(resolve '" (current-word ed) ")")
                                    :open-crossclj)))})
+
+(cmd/command {:command :inc-clojure.open-grimoire-url
+              :desc "IncClojure: Open grimoire page for current symbol"
+              :exec (fn []
+                      (let [ed (pool/last-active)]
+                        (eval-code ed
+                                   (str "(resolve '" (current-word ed) ")")
+                                   :open-grimoire)))})
 
 (comment
 
